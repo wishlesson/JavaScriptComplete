2675e40db7c3f6fccd286b1189f417a598d8f3fc
Shankar Kumar <shankar.kumar@geccomputersystemsllc.com>
2025-02-23 15:14:00 +0530

    Another commit

---
+++ varHoisting.js
@@ -0,0 +1,10 @@
+myName = "Chris";
+
+function logName() {
+    console.log(myName);
+}
+
+logName();
+
+var myName; // This code shall not give error
+//let myName; - This code shall give error
---
+++ variable2.js
@@ -0,0 +1,13 @@
+let a = 2;
+var varB = 200;
+console.log(a);
+function myFunction() {
+    //let a = 11;
+    console.log(a);
+    console.log(a + 1);
+    console.log(varB);
+    return a + 1;
+}
+console.log(myFunction());
+console.log(a);
+console.log(varB);
